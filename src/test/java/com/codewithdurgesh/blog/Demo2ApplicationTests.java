58f510b2bed52d364e2ac0c8ebae9ff2eb3e3cbf
Sukalyan Dash <sukalyandash97@gmail.com>
2023-09-04 11:58:06 +0530

    first commit

---
+++ src/test/java/com/codewithdurgesh/blog/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithdurgesh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
